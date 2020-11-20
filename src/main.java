bf0da03aa758f5b0e5dca38419934b8dacfeb9ba
Kujakuh <58704614+Kujakuh@users.noreply.github.com>
2020-11-20 23:41:48 +0100

    First

--- src/main.java
+++ src/main.java
@@ -1,3 +1,7 @@
+/*
+Programming final proyect developed by Mónica Treviño Fernández and Álvaro Vos Graciá.
+This is a simple command prompt based TicTacToe with some advanced settings.
+*/
 public class main {
     public static void main(String[] args){
         System.out.println("Hi");
