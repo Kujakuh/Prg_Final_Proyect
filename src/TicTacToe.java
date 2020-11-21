d1e8bdc6209e6662ba81f5f47425aca775c02b42
Kujakuh <58704614+Kujakuh@users.noreply.github.com>
2020-11-21 20:43:48 +0100

    Pse correction

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -3,7 +3,11 @@ Programming final proyect developed by Mónica Treviño Fernández and Álvaro V
 This is a simple command prompt based TicTacToe with some advanced settings.
 */
 
+import java.util.*;
+
 public class TicTacToe {
+
+    Scanner input = new Scanner(System.in);
 
     public static void main(String[] args){
         System.out.println( Data.ANSI_YELLOW + "Hi" + Data.ANSI_RESET);
