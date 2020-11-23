f3f428364b4734fec6e23f8330c7f620fb20d995
Kujakuh <avos202@gmail.com>
2020-11-23 19:49:22 +0100

    Correcion Menu

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -10,58 +10,42 @@ public class TicTacToe {
 
     Scanner input = new Scanner(System.in);
 
-    public static void main(String[] args){
-        //Para hacer el menú
+    public static void main(String[] args) {
+        // Para hacer el menú
         menu();
-        /*Scanner baia = new Scanner(System.in);
-        boolean exit = false;
-        int option;
-        while (!exit){
-            System.out.println("Choose an option:" +'\n'+
-                                "1 = Instructions"+ '\n'+
-                                "2 = Settings"+'\n'+
-                                "3 = Play"+'\n'+
-                                "4 = Exit");
-            option = baia.nextInt();
-         switch(option){
-             case 1: 
-             System.out.println("Instructions");
-             break;
-             case 2: 
-             System.out.println("Settings");
-             break;
-             case 3:
-             System.out.println("Play");
-             break;
-             case 4:
-             exit = true;
-             break;
-             default:
-             System.out.println("You can only choose a number from 1 to 4");
-         }
-
-        }*/
+        /*
+         * Scanner baia = new Scanner(System.in); boolean exit = false; int option;
+         * while (!exit){ System.out.println("Choose an option:" +'\n'+
+         * "1 = Instructions"+ '\n'+ "2 = Settings"+'\n'+ "3 = Play"+'\n'+ "4 = Exit");
+         * option = baia.nextInt(); switch(option){ case 1:
+         * System.out.println("Instructions"); break; case 2:
+         * System.out.println("Settings"); break; case 3: System.out.println("Play");
+         * break; case 4: exit = true; break; default:
+         * System.out.println("You can only choose a number from 1 to 4"); }
+         * 
+         * }
+         */
 
     }
-    public static int menu(){
+
+    public static int menu() {
         Scanner menu = new Scanner(System.in);
         int option = 0;
-        do{
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-        System.out.println("Choose an option:" +'\n'+
-                                "1 = Instructions"+ '\n'+
-                                "2 = Settings"+'\n'+
-                                "3 = Play"+'\n'+
-                                "4 = Exit");
+        do {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            System.out.println("Choose an option:" + '\n' + "1 = Instructions" + '\n' + "2 = Settings" + '\n'
+                    + "3 = Play" + '\n' + "4 = Exit");
             option = menu.nextInt();
-            /*if(menu.hasNextInt()){
-                option = menu.nextInt();
-            }
-            else {System.out.println("Try again, invalid input");}*/
-            if (option != 1&&option !=2&&option !=3&&option !=4){
+            /*
+             * if(menu.hasNextInt()){ option = menu.nextInt(); } else
+             * {System.out.println("Try again, invalid input");}
+             */
+            if (option != 1 && option != 2 && option != 3 && option != 4) {
                 System.out.println("Try again, invalid input");
-                Thread.sleep( 2000);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {e.printStackTrace();}
             }
         }while(option != 1&&option !=2&&option !=3&&option !=4);   
 return option;
