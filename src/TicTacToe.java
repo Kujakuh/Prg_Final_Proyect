185478bfde88a2cadfbaa5ac957e74a090461dee
Kujakuh <avos202@gmail.com>
2020-11-25 00:22:41 +0100

    Working menu and Instruc

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -8,8 +8,24 @@ import java.lang.Thread;
 
 public class TicTacToe {
 
+    public static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        menu();
+
+        while(true){
+
+            int stage = menu();
+        
+            switch(stage){
+            
+                case 1: instructions(); break;
+
+                case 4: System.exit(-1);
+
+                default: break;
+
+            }
+        }
     }
 
     // Terminal Cleaning
@@ -25,10 +41,19 @@ public class TicTacToe {
         } catch (InterruptedException e) {e.printStackTrace();}
     }
 
+    public static void print_matrix(char[][] Board){
+        
+        for(char[] row : Board){
+            for(char c : row){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
     // Menu display function, that will be returning the selected stage 
     public static int menu() {
 
-        Scanner menu = new Scanner(System.in);
         int option;
         do {
             // Terminal clear
@@ -41,12 +66,12 @@ public class TicTacToe {
                                                      '\n' + "4 = Exit");
             
             // Avoid data types errors, forbidding the user to give us a non numeric input
-            if(menu.hasNextInt()) option = menu.nextInt(); else option = 0; 
+            if(input.hasNextInt()) option = input.nextInt(); else option = 0; 
              
             // Check user input to only use the aviable options
             if (option != 1 && option != 2 && option != 3 && option != 4) {
                 
-                menu.nextLine();
+                input.nextLine();
                 System.out.println("Try again, invalid input");
                 sleep(2000);
                 
@@ -54,14 +79,15 @@ public class TicTacToe {
             // Check user input to only use the aviable options
         } while(option != 1 && option !=2 && option !=3 && option !=4);
         
-        // Scanner end
-        menu.close();
         return option;
     }
-    public static int Instructions() {
-        int option = 0;
-        if ( option == 1){
-            char[][] ExampleBoard = {
+
+
+    public static void instructions() {
+
+        int end_check = 0;
+
+        char[][] inst_board = {
                 {'┌', '─', '─', '─', '┬', '─', '─', '─', '┬', '─', '─', '─', '┐'},
                 {'|', ' ', '1', ' ', '|', ' ', '2', ' ', '|', ' ', '3', ' ', '|'},
                 {'├', '─', '─', '─', '┼', '─', '─', '─', '┼', '─', '─', '─', '┤'},
@@ -69,15 +95,33 @@ public class TicTacToe {
                 {'├', '─', '─', '─', '┼', '─', '─', '─', '┼', '─', '─', '─', '┤'},
                 {'│', ' ', '7', ' ', '|', ' ', '8', ' ', '|', ' ', '9', ' ', '|'},
                 {'└', '─', '─', '─', '┴', '─', '─', '─', '┴', '─', '─', '─', '┘'},};
+
+        do{
+        
+            cls();
+
             System.out.println("The game is played on a grid that's 3x3 squares.");
-            System.out.println(ExampleBoard);
+
+            print_matrix(inst_board);
+
             System.out.println("Players take turns putting their marks in empty squares."+ '\n'+
-             "The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner." +'\n'+
-             "When all 9 squares are full, the game is over."+'\n');
-             System.out.println("To play you just have to say in which square you want to place your mark when your turn comes"); 
-        }
-        return option;
+                               "The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner." +'\n'+
+                               "When all 9 squares are full, the game is over."+'\n');
+
+            System.out.println("To play you just have to say in which square you want to place your mark when your turn comes"); 
+            
+            System.out.println("Press 1 to go back to the menu");
+
+            input.nextLine();
+
+            if(input.hasNextInt()) {
+                
+                end_check = input.nextInt();
+                if(end_check != 1) { System.out.println("Invalid input, try again."); sleep(1250);}
+
+            } else { System.out.println("Invalid input, try again."); sleep(1250);}
+
+        } while(end_check != 1);
+
     }
 }
-
-
