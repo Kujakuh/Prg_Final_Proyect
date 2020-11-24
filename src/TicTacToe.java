e2d878fb236ba9c05fdc478303ad2d723a6d16b5
Kujakuh <avos202@gmail.com>
2020-11-24 01:28:28 +0100

    Menu Func Pre-Finished

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -15,25 +15,40 @@ public class TicTacToe {
         menu();
     }
 
+    public static void cls() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static void sleep(int i) {
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {e.printStackTrace();}
+    }
+
     public static int menu() {
         Scanner menu = new Scanner(System.in);
         int option;
         do {
-            System.out.print("\033[H\033[2J");
-            System.out.flush();
-            System.out.println("Choose an option:" + '\n' + "1 = Instructions" + '\n' + "2 = Settings" + '\n'
-                    + "3 = Play" + '\n' + "4 = Exit");
+            cls();
+            
+            System.out.println("Choose an option:" + '\n' + "1 = Instructions" +
+                                                     '\n' + "2 = Settings" + 
+                                                     '\n' + "3 = Play" + 
+                                                     '\n' + "4 = Exit");
             
             if(menu.hasNextInt()) option = menu.nextInt(); else option = 0;
              
             if (option != 1 && option != 2 && option != 3 && option != 4) {
+                
                 menu.nextLine();
                 System.out.println("Try again, invalid input");
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {e.printStackTrace();}
+                sleep(2000);
+                
             }
+
         } while(option != 1 && option !=2 && option !=3 && option !=4);
+        
         menu.close();
         return option;
     }
